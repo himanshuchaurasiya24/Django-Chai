7afa85deafed5f62c441dcff5227dd709d8e7ffb
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-09-04 21:13:37 +0530

    tweet_not showing in tweet_list.html

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index
+from .views import *
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('index/', index)
-    path('', index, name='index')
+    path('', tweet_list, name='tweet_list'),
+    path('create/', tweet_create, name='tweet_create'),
+    path('<int:tweet_id>/delete/', tweet_delete, name='tweet_delete'),
+    path('<int:tweet_id>/edit/', tweet_edit, name='tweet_edit'),
 ]
